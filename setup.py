cd3bb0401714c51a3b67f4f102bd109411b63caa
Maxime Haineault <max@motion-m.ca>
2012-02-11 00:03:57 -0500

    Updated .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "duke-website",
-    version = "1.0",
+    version = "0.0.1",
     url = 'https://github.com/h3/duke-website',
     license = 'BSD',
     description = "Duke website",
     author = 'Maxime Haineault',
     packages = find_packages('eggs'),
-    package_dir = {'': 'eggs'},
+    package_dir = {'': '.'},
     install_requires = ['distribute'],
 )
