20799d5ebd15c9799d6cf8fc130796be57c58814
Maxime Haineault <max@motion-m.ca>
2012-02-14 23:52:46 -0500

    Updated templates and settings

--- dukewebsite/settings.py
+++ dukewebsite/settings.py
@@ -89,13 +89,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'website.urls'
+ROOT_URLCONF = 'dukewebsite.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), "templates"),
+    os.path.join(os.path.dirname(__file__), "templates/"),
 )
 
 INSTALLED_APPS = (
@@ -109,7 +109,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'dukewebsite.website',
+   #'dukewebsite.website',
 )
 
 # A sample logging configuration. The only tangible logging
